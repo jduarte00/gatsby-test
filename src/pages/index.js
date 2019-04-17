3537a24f524bf96bdca9192ea90198cb3adb313c
Jose Duarte <dumejo92@gmail.com>
2019-04-17 18:21:34 -0500

    try a static query on a page to test markdown queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -9,6 +9,28 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
+    <StaticQuery
+      query={graphql`
+        query theQuery {
+          allMarkdownRemark {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  slug
+                  date(formatString: "YY-MM-DD")
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <>
+          <p>{data.allMarkdownRemark.edges[0].node.frontmatter.date}</p>
+        </>
+      )}
+    />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
