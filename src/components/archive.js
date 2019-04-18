a190b447484aa1666bd2bc6a352e1b107f0aa9df
Jose Duarte <dumejo92@gmail.com>
2019-04-17 19:34:34 -0500

    refactor the query outside of the component

--- src/components/archive.js
+++ src/components/archive.js
@@ -2,24 +2,26 @@ import React from "react"
 
 import { StaticQuery, graphql } from "gatsby"
 
+const POST_ARCHIVE_QUERY = graphql`
+  query BlogPostArchive {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
 function Archive() {
   return (
     <div>
       <StaticQuery
-        query={graphql`
-          query BlogPostArchive {
-            allMarkdownRemark {
-              edges {
-                node {
-                  frontmatter {
-                    title
-                    slug
-                  }
-                }
-              }
-            }
-          }
-        `}
+        query={POST_ARCHIVE_QUERY}
         render={({ allMarkdownRemark }) => (
           <>
             <aside>
