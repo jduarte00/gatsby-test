040255d53f4665d91a586ecf782c56efb6d9f239
Jose Duarte <dumejo92@gmail.com>
2019-04-18 09:28:06 -0500

    Implement styled components on the index and header

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,25 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styled from "styled-components"
+
+const HeaderWrapper = styled.header`
+  background: coral;
+  margin-bottom: 1.45rem;
+  img {
+    margin-bottom: 0px;
+  }
+`
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1rem;
+`
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -27,8 +32,8 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </header>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
